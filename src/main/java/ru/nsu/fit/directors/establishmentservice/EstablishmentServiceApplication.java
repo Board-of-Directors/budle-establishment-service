f95628a842b50ae2302389f8cd96969fdf2c7734
Fagor <oleg-veber@mail.ru>
2024-02-28 23:33:47 +0700

    Delete web clients configuration

--- src/main/java/ru/nsu/fit/directors/establishmentservice/EstablishmentServiceApplication.java
+++ src/main/java/ru/nsu/fit/directors/establishmentservice/EstablishmentServiceApplication.java
@@ -1,23 +1,14 @@
 package ru.nsu.fit.directors.establishmentservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@OpenAPIDefinition(servers = {
-    @Server(
-        description = "This is the localhost",
-        url = "localhost:8080/establishment"
-    ),
-    @Server(
-        description = "This is the real server",
-        url = "80.87.200.185/establishment"
-    )
-})
+@OpenAPIDefinition
 public class EstablishmentServiceApplication {
 
     public static void main(String[] args) {
